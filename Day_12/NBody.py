26ec9610ef1f9121ee995e3bbcf8d5d17dfebad2
David Pilger <dpilger26@gmail.com>
2019-12-12 21:48:04 -0700

    Completed Day 12

--- Day_12/NBody.py
+++ Day_12/NBody.py
@@ -1,11 +1,13 @@
 """
 The N-Body problem
 """
-from copy import deepcopy
+import copy
 import re
 import os
 import pathlib
 from typing import List
+
+import numpy as np
 
 INPUT_FILE = os.path.join(pathlib.Path(__file__).parent, 'input.txt')
 
@@ -15,6 +17,9 @@ class Vec3:
     Simple Vec3 class
     """
     def __init__(self, x: int = 0, y: int = 0, z: int = 0):
+        """
+        Constructor
+        """
         self.x = x
         self.y = y
         self.z = z
@@ -31,7 +36,7 @@ class Vec3:
         """
         return self.__str__()
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Vec3'):
         """
         Equality operator
         """
@@ -95,7 +100,10 @@ class Body:
     """
     Simple body class
     """
-    def __init__(self, position: Vec3 = Vec3(), velocity: Vec3 = Vec3()):
+    def __init__(self, position: Vec3, velocity: Vec3):
+        """
+        Constructor
+        """
         self.position = position
         self.velocity = velocity
 
@@ -110,6 +118,12 @@ class Body:
         repr method
         """
         return self.__str__()
+
+    def __eq__(self, other: 'Body'):
+        """
+        Equality operator
+        """
+        return self.position == other.position and self.velocity == other.velocity
 
     def potentialEnergy(self) -> int:
         """
@@ -129,164 +143,228 @@ class Body:
         """
         return self.potentialEnergy() * self.kineticEnergy()
 
-    def updateVelocity(self, other: 'Body') -> None:
-        """
-        Updates the velocity based of the simple gravity model
-        """
-        if self.position.x < other.position.x:
-            print('updating +x')
-            self.velocity.right()
-            other.velocity.left()
-        elif self.position.x > other.position.x:
-            print('updating -x')
-            self.velocity.left()
-            other.velocity.right()
-
-        if self.position.y < other.position.y:
-            print('updating +y')
-            self.velocity.up()
-            other.velocity.down()
-        elif self.position.y > other.position.y:
-            print('updating -y')
-            self.velocity.down()
-            other.velocity.up()
-
-        if self.position.z < other.position.z:
-            print('updating +z')
-            self.velocity.forward()
-            other.velocity.back()
-        elif self.position.z > other.position.z:
-            print('updating -z')
-            self.velocity.back()
-            other.velocity.forward()
-
-        print(f'\t\t{self}')
-        print(f'\t\t{other}')
-
-    def updatePosition(self) -> None:
+    def updatePosition(self) -> 'Body':
         """
         Updates the position of the body
         """
         self.position += self.velocity
+
+        return self
+
+
+class NBody:
+    """
+    Simple Nbody class
+    """
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.bodies = list()
+        self.current = 0
+
+    def __getitem__(self, index):
+        """
+        item at index
+        """
+        return self.bodies[index]
+
+    def __len__(self):
+        """
+        len operator
+        """
+        return self.size()
+
+    def __iter__(self):
+        """
+        iter
+        """
+        return self
+
+    def __next__(self):
+        """
+        next
+        """
+        self.current += 1
+        if self.current < self.size():
+            return self.bodies[self.current]
+        raise StopIteration
+
+    def __str__(self):
+        """
+        String method
+        """
+        string = ''
+        for body in self.bodies:
+            string += f'{body}\n'
+
+        return string
+
+    def __repr__(self):
+        """
+        repr method
+        """
+        return self.__str__()
+
+    def addBody(self, body: Body) -> None:
+        """
+        Adds a body to the system
+        """
+        self.bodies.append(body)
+
+    def size(self) -> int:
+        """
+        size
+        """
+        return len(self.bodies)
+
+    def updateVelocity(self, index1, index2) -> None:
+        """
+        Updates the velocity based of the simple gravity model
+        """
+        if self.bodies[index1].position.x < self.bodies[index2].position.x:
+            self.bodies[index1].velocity.right()
+            self.bodies[index2].velocity.left()
+        elif self.bodies[index1].position.x > self.bodies[index2].position.x:
+            self.bodies[index1].velocity.left()
+            self.bodies[index2].velocity.right()
+
+        if self.bodies[index1].position.y < self.bodies[index2].position.y:
+            self.bodies[index1].velocity.up()
+            self.bodies[index2].velocity.down()
+        elif self.bodies[index1].position.y > self.bodies[index2].position.y:
+            self.bodies[index1].velocity.down()
+            self.bodies[index2].velocity.up()
+
+        if self.bodies[index1].position.z < self.bodies[index2].position.z:
+            self.bodies[index1].velocity.forward()
+            self.bodies[index2].velocity.back()
+        elif self.bodies[index1].position.z > self.bodies[index2].position.z:
+            self.bodies[index1].velocity.back()
+            self.bodies[index2].velocity.forward()
+
+    def updateVelocities(self) -> None:
+        """
+        Updates all of the velocities
+        """
+        for idx1 in range(self.size() - 1):
+            for idx2 in range(idx1 + 1, self.size()):
+                self.updateVelocity(idx1, idx2)
+
+    def updatePositions(self) -> None:
+        """
+        Updates all of the positions
+        """
+        for idx in range(self.size()):
+            self.bodies[idx].updatePosition()
+
+    def update(self):
+        """
+        Updates the velocities and positions
+        """
+        self.updateVelocities()
+        self.updatePositions()
+
+    def totalEnergy(self) -> int:
+        """
+        Returns the total energy of the system
+        """
+        totalEnergy = 0
+        for body in self.bodies:
+            totalEnergy += body.totalEnergy()
+
+        return totalEnergy
+
+    def xAxisEqual(self, otherBodies: List[Body]) -> bool:
+        """
+        checks if the bodies are equal for the x axis
+        """
+        if ([body.position.x for body in self.bodies] == [body.position.x for body in otherBodies] and
+                not np.any([body.velocity.x for body in self.bodies])):
+            return True
+
+    def yAxisEqual(self, otherBodies: List[Body]) -> bool:
+        """
+        checks if the bodies are equal for the y axis
+        """
+        if ([body.position.y for body in self.bodies] == [body.position.y for body in otherBodies] and
+                not np.any([body.velocity.y for body in self.bodies])):
+            return True
+
+    def zAxisEqual(self, otherBodies: List[Body]) -> bool:
+        """
+        checks if the bodies are equal for the z axis
+        """
+        if ([body.position.z for body in self.bodies] == [body.position.z for body in otherBodies] and
+                not np.any([body.velocity.z for body in self.bodies])):
+            return True
 
 
 def calcTotalEnergy(numSteps: int, verboseOutput: bool = False) -> None:
     """
     Calculates the total energy of the moons after input steps
     """
-    moons = readInput()
+    nBody = readInput()
 
     if verboseOutput:
-        print(f'After 0 steps')
-        for moon in moons:
-            print(moon)
+        print(f'After 0 steps\n{nBody}')
 
     for step in range(numSteps):
-        for idx1 in range(len(moons) - 1):
-            for idx2 in range(idx1 + 1, len(moons)):
-                moons[idx1].updateVelocity(moons[idx2])
-                print(f'\t{moons[idx1]}')
-                print(f'\t{moons[idx2]}')
-
-        for moon in moons:
-            print(moon)
-            moon.updatePosition()
-            print(moon)
+        nBody.update()
 
         if verboseOutput:
-            print(f'After {step + 1} steps')
-            for moon in moons:
-                print(moon)
+            print(f'After {step + 1} steps\n{nBody}')
 
-    totalEnergy = 0
-    for moon in moons:
-        totalEnergy += moon.totalEnergy()
-
-    print(f'Total Energy = {totalEnergy}')
+    print(f'Total Energy = {nBody.totalEnergy()}')
 
 
-# def calcStepsToHistory(verboseOutput: bool = False) -> None:
-#     """
-#     Calculates the number of steps before the moons return to a position
-#     they have already been in before
-#     """
-#     moonCoordinates = readInput()
-#     moonVelocities = [Vec3() for _ in range(len(moonCoordinates))]
-#
-#     if verboseOutput:
-#         print(f'After 0 steps')
-#         for idx, moonCoordinate in enumerate(moonCoordinates):
-#             print(f'pos={moonCoordinate},\tvel={moonVelocities[idx]}')
-#
-#     positionHistory = list()
-#     positionHistory.append(deepcopy(moonCoordinates))
-#
-#     velocityHistory = list()
-#     velocityHistory.append(deepcopy(moonVelocities))
-#
-#     numSteps = 1
-#     while True:
-#         print(f'Step {numSteps}')
-#         for idx1 in range(len(moonCoordinates) - 1):
-#             for idx2 in range(idx1 + 1, len(moonCoordinates)):
-#                 if moonCoordinates[idx1].x < moonCoordinates[idx2].x:
-#                     moonVelocities[idx1].x += 1
-#                     moonVelocities[idx2].x -= 1
-#                 elif moonCoordinates[idx1].x > moonCoordinates[idx2].x:
-#                     moonVelocities[idx1].x -= 1
-#                     moonVelocities[idx2].x += 1
-#
-#                 if moonCoordinates[idx1].y < moonCoordinates[idx2].y:
-#                     moonVelocities[idx1].y += 1
-#                     moonVelocities[idx2].y -= 1
-#                 elif moonCoordinates[idx1].y > moonCoordinates[idx2].y:
-#                     moonVelocities[idx1].y -= 1
-#                     moonVelocities[idx2].y += 1
-#
-#                 if moonCoordinates[idx1].z < moonCoordinates[idx2].z:
-#                     moonVelocities[idx1].z += 1
-#                     moonVelocities[idx2].z -= 1
-#                 elif moonCoordinates[idx1].z > moonCoordinates[idx2].z:
-#                     moonVelocities[idx1].z -= 1
-#                     moonVelocities[idx2].z += 1
-#
-#         for idx, moonCoordinate in enumerate(moonCoordinates):
-#             moonCoordinate += moonVelocities[idx]
-#
-#         if verboseOutput:
-#             print(f'After {numSteps + 1} steps')
-#             for idx, moonCoordinate in enumerate(moonCoordinates):
-#                 print(f'pos={moonCoordinate},\tvel={moonVelocities[idx]}')
-#
-#         if moonCoordinates in positionHistory:
-#             idx = positionHistory.index(moonCoordinates)
-#             if moonVelocities == velocityHistory[idx]:
-#                 break
-#
-#         positionHistory.append(deepcopy(moonCoordinates))
-#         velocityHistory.append(deepcopy(moonVelocities))
-#         numSteps += 1
-#
-#     print(f'Number of steps to history = {numSteps}')
+def calcStepsToHistory() -> None:
+    """
+    Calculates the number of steps before the moons return to a position
+    they have already been in before
+    """
+    nBody = readInput()
+
+    initial = copy.deepcopy(nBody.bodies)
+    axisStops = [None, None, None]
+
+    step = 0
+    while not np.all(axisStops):
+        nBody.update()
+        step += 1
+
+        if axisStops[0] is None and nBody.xAxisEqual(initial):
+            axisStops[0] = step
+
+        if axisStops[1] is None and nBody.yAxisEqual(initial):
+            axisStops[1] = step
+
+        if axisStops[2] is None and nBody.zAxisEqual(initial):
+            axisStops[2] = step
+
+    axisStops = np.array(axisStops, dtype=np.uint64)
+    numSteps = np.lcm(np.lcm(axisStops[0], axisStops[1]), axisStops[2])
+    print(f'Number of steps to history = {numSteps}')
 
 
-def readInput() -> List[Body]:
+def readInput() -> NBody:
     """
     Reads the input file
     """
     tokenizer = re.compile('<x=(.+), y=(.+), z=(.+)>')
 
-    bodies = list()
+    nBody = NBody()
     with open(INPUT_FILE) as f:
         line = f.readline()
         while line != '':
             x, y, z = tokenizer.findall(line)[0]
-            bodies.append(Body(position=Vec3(x=int(x), y=int(y), z=int(z))))
+            nBody.addBody(Body(position=Vec3(x=int(x), y=int(y), z=int(z)),
+                               velocity=Vec3(x=0, y=0, z=0)))
             line = f.readline()
 
-    return bodies
+    return nBody
 
 
 if __name__ == '__main__':
-    calcTotalEnergy(numSteps=1000, verboseOutput=True)
-    # calcStepsToHistory(verboseOutput=False)
+    # calcTotalEnergy(numSteps=1000, verboseOutput=True)
+    calcStepsToHistory()
